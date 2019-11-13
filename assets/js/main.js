1514d5f31c2711d2949eeb55d1fade6968634725
dev-prime99 <prime99@icloud.com>
2019-11-13 21:00:25 +0100

    Milestone 2 completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,11 +26,52 @@ $(document).keydown(function (e){
 
 // funione stampa testo
 function text(){
-  var val = $("#myinput").val();
-  if ( !$("#myinput").val() ) {
-    alert("inserisci un testo");
+
+  var messaggio = $("#myinput").val();
+  var risposta = "Ok";
+
+  var elmentmsg = $("#template_user .user").clone();
+  var messcontact = $("#template_contact .cont").clone();
+
+  var dt = new Date();
+  var time = dt.getHours() + ":" + dt.getMinutes();
+
+  elmentmsg.find(".outtext").text(messaggio);
+  elmentmsg.find(".outtime").text(time);
+  messcontact.find(".outtext").text(risposta);
+  messcontact.find(".outtime").text(time);
+
+if ( !$("#myinput").val() ) {
+  alert("inserisci un testo");
   } else{
-    document.getElementById('ii').innerHTML += "<div class='user'><div class='bs_graf_txt'><p>" + val + "<span>14:44</span></p></div></div>";
-    $("#myinput").val("");
+  $(".contact-mex").append(elmentmsg);
+  setTimeout(function () {
+    $(".contact-mex").append(messcontact)
+  }, 1000);
+
+  $("#myinput").val("");
   }
 }
+
+
+$("#search_contact").keyup(function(){
+
+  var text = $("#search_contact").val().toLowerCase();
+
+  var list_contact = $(".contact-cred h4:first-child");
+
+  list_contact.each(function() {
+
+    var element = $(this);
+
+    var name = element.text().toLowerCase();
+
+    var parent = element.parents(".chat");
+
+    if (name.includes(text)) {
+      parent.show();
+    } else{
+      parent.hide();
+    }
+  });
+});
